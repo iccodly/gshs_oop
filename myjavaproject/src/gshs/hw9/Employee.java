084fca5f3088ef60b92fb4947a210ded062b7dd4
iccodly <ljh271828@gmail.com>
2024-03-31 20:16:39 +0900

    코드만 남김

---
+++ myjavaproject/src/gshs/hw9/Employee.java
@@ -0,0 +1,15 @@
+package gshs.hw9;
+
+public class Employee {
+	public String name;
+	public int age;
+
+	public Employee(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void print() {
+		System.out.println("* 사원의 이름은 " + this.name + "이고, 나이는 " + this.age + " 입니다.");
+	}
+}
---
+++ myjavaproject/src/gshs/hw9/OverridingTest.java
@@ -0,0 +1,10 @@
+package gshs.hw9;
+
+public class OverridingTest {
+	public static void main(String[] args) {
+		Employee employee = new Employee("Kim min", 25);
+		Manager manager = new Manager("Lee Joon", 30, "Project Manager");
+		employee.print();
+		manager.print();
+	}
+}
